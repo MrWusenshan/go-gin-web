1d4ba3747b1c1a5c24dc3bc32029cdb752e57d2f
MrWusenshan <569962103@qq.com>
2021-09-21 14:54:49 +0800

    add: create user login api

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"crypto/bcrypt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -43,17 +44,66 @@ func UserRegister(ctx *gin.Context) {
 	if len(name) == 0 {
 		name = utils.RandomUsername(10)
 	}
-
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"code":    500,
+			"message": "用户密码加密出错了!",
+		})
+		return
+	}
 	newUser := models.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hashPassword),
 	}
 	db.Create(&newUser)
 
 	ctx.JSON(http.StatusOK, gin.H{
 		"code":    200,
 		"message": "注册成功!",
+	})
+}
+
+func UserLogin(ctx *gin.Context) {
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "手机号码必须为11位!",
+		})
+		return
+	}
+	db := common.GetDb()
+	var user models.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "该手机号未注册!",
+		})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": "密码错误!",
+		})
+		return
+	}
+
+	token := 1111
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{
+			"token": token,
+		},
+		"message": "登录成功!",
 	})
 }
 
--- routers/user.go
+++ routers/user.go
@@ -8,4 +8,5 @@ import (
 // User 用户功能模块相关路由
 func User(router *gin.Engine) {
 	router.POST("/api/auth/register", controller.UserRegister)
+	router.POST("/api/auth/login", controller.UserLogin)
 }
