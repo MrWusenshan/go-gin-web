5d85ede30a039b0ec03ff1bd8593859ab87fb010
MrWusenshan <569962103@qq.com>
2021-09-21 12:08:40 +0800

    add: the web service init

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := gin.Default()
+	app.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"code":    200,
+			"message": "pang",
+		})
+	})
+	panic(app.Run(":8000"))
 }
